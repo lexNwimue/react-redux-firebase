66089242fd53e17dbbcc422ac7fe4d2e8f91d427
lexNwimue <nwimueprogress@gmail.com>
2022-05-29 13:51:20 +0100

    Initial commit. Created files (some empty) for all components. Implemented Navbar and its subcomponents, Dashboard and its subcomponent albeit with dummy data

---
+++ src/components/layout/SignedInLinks.js
@@ -0,0 +1,21 @@
+import { NavLink } from "react-router-dom";
+
+const SignedInLinks = () => {
+  return (
+    <ul className="right">
+      <li>
+        <NavLink to="/">New Project</NavLink>
+      </li>
+      <li>
+        <NavLink to="/">Logout</NavLink>
+      </li>
+      <li>
+        <NavLink className="btn btn-floating pink lighten-1" to="/">
+          LN
+        </NavLink>
+      </li>
+    </ul>
+  );
+};
+
+export default SignedInLinks;
---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,29 @@
+const ProjectList = () => {
+  return (
+    <div className="project-list section">
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by lex Nwimue</p>
+          <p className="grey-text">Jan. 12, 2022</p>
+        </div>
+      </div>
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by lex Nwimue</p>
+          <p className="grey-text">Jan. 12, 2022</p>
+        </div>
+      </div>
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by lex Nwimue</p>
+          <p className="grey-text">Jan. 12, 2022</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectList;
